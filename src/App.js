b47a1a8e63e80e35fd87762c9d1bf00e61ad4455
mbrsagor <mbrsagor@gmail.com>
2022-01-28 00:05:57 +0600

    New: Add header and contentbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './scss/app.scss';
-import Sidebar from './components/sidebar'
+import Sidebar from './components/sidebar';
+import Layout from './components/layout'
 
 function App() {
   return (
     <div className="container">
       <Sidebar />
+      <Layout />
     </div>
   );
 }
---
+++ src/components/layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Layout() {
+  return (
+    <div className="main">
+      <div className="top_bar">
+        <div className="toggle"></div>
+        <div className="user">
+              <img width="60" height="60" src="https://cutout-frontend.vercel.app/dist/img/user2-160x160.jpg" alt="img" />
+          </div>
+      </div>
+    </div>
+  );
+}
